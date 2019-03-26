eb45ff59f08d8984cb59f6346a65de2f7d880937
rsookram <rashadsookram@gmail.com>
2019-03-26 18:59:17 -0400

    Rename month module to cal

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 extern crate chrono;
 
-mod month;
+mod cal;
 
+use cal::Month;
 use chrono::prelude::*;
 
 fn main() {
     let now = Local::now().naive_local().date();
-    let m = month::Month::new(now.year(), now.month()).expect("invalid time");
+    let m = Month::new(now.year(), now.month()).expect("invalid time");
     println!("{}", m);
 }
